1b164b098638287a7a3d2eba0de53c1ea32c31af
Salman-sajib <sfsajib@gmail.com>
2025-01-19 13:26:33 +0600

    added all the animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,53 @@
+import { useState } from "react";
 import ScrollAnimation from "./components/ScrollAnimation";
+import { motion, useScroll } from "motion/react";
+import { useEffect } from "react";
+import { ChevronsUp } from "lucide-react";
 
 function App() {
+  const { scrollYProgress } = useScroll();
+  const [showButton, setShowButton] = useState(false);
+
+  // show back-to-top button when scrolled down
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
-    <div className="min-h-[300vh]">
+    <div className="min-h-[300vh] relative">
+      {/* Progress bar */}
+      <motion.div
+        className="fixed top-0 left-0 h-2 w-full bg-red-400 z-50 origin-left"
+        style={{ scaleX: scrollYProgress }}
+      />
+
+      {/* back to top button */}
+      {showButton && (
+        <button
+          onClick={scrollToTop}
+          className="fixed bottom-5 right-5 border border-gray-300 hover:border-gray-800 p-3 rounded-full focus:outline-none animate-bounce"
+        >
+          <ChevronsUp size={24} color="#111827" />
+        </button>
+      )}
+
       <main>
-        <div className="min-h-[100vh]"></div>
+        <div className="min-h-[100vh]">
+          <h1 className="text-7xl font-semibold text-center pt-10">Scroll</h1>
+        </div>
         <ScrollAnimation />
         <div className="min-h-[100vh]"></div>
       </main>
--- src/components/ScrollAnimation.jsx
+++ src/components/ScrollAnimation.jsx
@@ -1,7 +1,25 @@
+import {
+  motion,
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+} from "motion/react";
+
 function ScrollAnimation() {
+  const { scrollY } = useScroll();
+
+  useMotionValueEvent(scrollY, "change", (latestValue) => {
+    console.log(latestValue);
+  });
+
+  const scale = useTransform(scrollY, [230, 1000], [1, 1.5]);
+
   return (
-    <div className="min-h[100vh] flex items-center justify-center">
-      <div className="w-[600px] h-[500px] bg-blue-600 rounded-[1rem]"></div>
+    <div className="min-h-[100vh] flex items-center justify-center">
+      <motion.div
+        style={{ scale }}
+        className="w-[60vw] h-[500px] bg-blue-700 rounded-[1rem]"
+      ></motion.div>
     </div>
   );
 }
