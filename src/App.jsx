09ca2eabd809a24ded6ea3f3623b2c4358566609
Salman-sajib <sfsajib@gmail.com>
2025-01-19 12:17:03 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ScrollAnimation from "./components/ScrollAnimation";
+
+function App() {
+  return (
+    <div className="min-h-[300vh]">
+      <main>
+        <div className="min-h-[100vh]"></div>
+        <ScrollAnimation />
+        <div className="min-h-[100vh]"></div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ScrollAnimation.jsx
@@ -0,0 +1,9 @@
+function ScrollAnimation() {
+  return (
+    <div className="min-h[100vh] flex items-center justify-center">
+      <div className="w-[600px] h-[500px] bg-blue-600 rounded-[1rem]"></div>
+    </div>
+  );
+}
+
+export default ScrollAnimation;
